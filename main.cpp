cf2ce768664a5273627c201713dd710e696b8e2f
rrizun <rrizun@gmail.com>
2020-03-28 10:53:26 -0700

    checkpoint

--- main.cpp
+++ main.cpp
@@ -15,6 +15,30 @@
 
 #include "dnsmasq.pb.h"
 
+//#include <stdio.h>
+//#include <stdlib.h>
+//#include <sys/wait.h>
+//#include <unistd.h>
+//#include <sys/prctl.h>
+//void print_trace() {
+//    char pid_buf[30];
+//    sprintf(pid_buf, "%d", getpid());
+//    char name_buf[512];
+//    name_buf[readlink("/proc/self/exe", name_buf, sizeof(name_buf)-1)]=0;
+//
+//    prctl(PR_SET_PTRACER, PR_SET_PTRACER_ANY, 0, 0, 0);
+//
+//    int child_pid = fork();
+//    if (!child_pid) {
+////        dup2(2,1); // redirect output to stderr
+////        fprintf(stdout,"stack trace for %s pid=%s\n",name_buf,pid_buf);
+//        execlp("gdb", "gdb", "--batch", "-n", "-ex", "thread", "-ex", "bt", name_buf, pid_buf, NULL);
+////        abort(); /* If gdb failed to start */
+//    } else {
+//        waitpid(child_pid,NULL,0);
+//    }
+//}
+
 using namespace std;
 
 inline string dostring() {
@@ -26,69 +50,86 @@ inline string dostring(T first, U ... second) {
   return boost::lexical_cast<string>(first)+" "+dostring(second...);
 }
 
-#define LOG(...) do{printf("%s\n", dostring(time(0),__PRETTY_FUNCTION__,__FILE__,__LINE__,##__VA_ARGS__).c_str());}while(0)
-#define THROW(...) do{throw runtime_error(dostring(time(0),__PRETTY_FUNCTION__,__FILE__,__LINE__,##__VA_ARGS__));}while(0)
+#define LOGX(...) do{printf("%s\n", dostring(time(0),__PRETTY_FUNCTION__,__FILE__,__LINE__,##__VA_ARGS__).c_str());}while(0)
+
+//printf("%s\n", dostring(time(0),__PRETTY_FUNCTION__,__FILE__,__LINE__,##__VA_ARGS__).c_str());\
+//  for (auto it : boost::stacktrace::stacktrace())\
+//    cout << to_string(it) << endl;\
+
+#define THROWX(...) do{\
+  stringstream e;\
+  e << dostring(__VA_ARGS__);\
+  e << boost::stacktrace::stacktrace();\
+  throw runtime_error(e.str());}while(0)
+
+//#define THROWX(...) do{\
+//  printf("######################################################################\n");\
+//  printf("%s\n", dostring(time(0),__PRETTY_FUNCTION__,__FILE__,__LINE__,##__VA_ARGS__).c_str());\
+//  cout << boost::stacktrace::stacktrace() << endl;\
+//  printf("######################################################################\n");\
+//  throw runtime_error(dostring(time(0),__PRETTY_FUNCTION__,__FILE__,__LINE__,##__VA_ARGS__));}while(0)
 
 vector<dnsmasq_lease> get_leases(string dnsmasq_leases) {
   vector<dnsmasq_lease> leases;
   return leases;
 }
 
-int baz() {
-  cout << boost::stacktrace::stacktrace() << endl;
-  for (auto it : boost::stacktrace::stacktrace()) {
-    LOG(it);
-  }
-  THROW("baz");
+int baz(string fd) {
+  THROWX("Oof! something really bad happened", "fd", fd);
 }
-int bar() {
-  return baz();
+int bar(int a) {
+  return baz("1");
 }
 int foo() {
-  return bar();
+  return bar(1);
 }
 
 int main() {
 
-  LOG("hello", 123);
+  try {
 
-  foo();
+    LOGX("hello", 123);
 
-  for (auto entry : boost::filesystem::directory_iterator("/tmp")) {
+    foo();
 
-    if (fnmatch("create_ap.*.conf.*", entry.path().filename().c_str(), 0) == 0) {
-      LOG(entry);
+    for (auto entry : boost::filesystem::directory_iterator("/tmp")) {
 
-      boost::filesystem::ifstream in(boost::filesystem::path(entry).append("dnsmasq.leases"));
+      if (fnmatch("create_ap.*.conf.*", entry.path().filename().c_str(), 0) == 0) {
+        LOGX(entry);
 
-      string line;
-      while (getline(in, line)) {
-//        stringstream linestream(line);
+        boost::filesystem::ifstream in(boost::filesystem::path(entry).append("dnsmasq.leases"));
 
-//        log(line);
-        int index = -1;
-        dnsmasq_lease lease;
-        boost::char_separator<char> sep{" "};
-        boost::tokenizer<boost::char_separator<char>> tok{line, sep};
-        for (auto it : tok) {
-//          log(it);
+        string line;
+        while (getline(in, line)) {
+  //        stringstream linestream(line);
 
-          ++index;
-          if (index == 0) {
-            lease.mutable_expires()->set_seconds(
-                boost::lexical_cast<::google::protobuf::int64>(it));
-          } else {
-            lease.GetReflection()->SetString(&lease, lease.GetDescriptor()->field(index), it);
+  //        log(line);
+          int index = -1;
+          dnsmasq_lease lease;
+          boost::char_separator<char> sep{" "};
+          boost::tokenizer<boost::char_separator<char>> tok{line, sep};
+          for (auto it : tok) {
+            ++index;
+            if (index == 0) {
+              lease.mutable_expires()->set_seconds(
+                  boost::lexical_cast<::google::protobuf::int64>(it));
+            } else {
+              lease.GetReflection()->SetString(&lease, lease.GetDescriptor()->field(index), it);
+            }
           }
+
+          LOGX(lease.DebugString());
+
         }
 
-        LOG(lease.DebugString());
 
       }
 
-
     }
 
+  } catch (const exception &e) {
+    LOGX("what", e.what());
+    LOGX("phew!");
   }
 
   return 0;
